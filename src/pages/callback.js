3184023e2c91f9d01bcc9b6bbcc433c14bcf3967
nyasharonaldo <nyasharonaldo@gmail.com>
2020-02-20 23:47:57 +0000

    navigation error

--- src/pages/callback.js
+++ src/pages/callback.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { handleAuthentication } from "../utils/auth"
-import { navigate } from "gatsby"
+import { navigate } from "@reach/router"
 const Callback = () => {
   handleAuthentication()
   navigate('/blog')
