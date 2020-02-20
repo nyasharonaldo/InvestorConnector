e66d75ac6fdfbc82624841a176857066aa78de99
nyasharonaldo <nyasharonaldo@gmail.com>
2020-02-20 23:40:40 +0000

    fixed callback stuck error

--- src/pages/callback.js
+++ src/pages/callback.js
@@ -1,9 +1,9 @@
 import React from "react"
 import { handleAuthentication } from "../utils/auth"
-
+import {navigate} from 'gatsby'
 const Callback = () => {
   handleAuthentication()
-
+  nagivate('/blog')
   return <p>Loading...</p>
 }
 
